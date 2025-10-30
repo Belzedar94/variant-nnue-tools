da7e5281ff93e4e7551191b9a6395d76e9266a9f
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2025-10-30 14:41:55 +0100

    Merge remote-tracking branch 'upstream/codex/implement-battle-of-the-kings-variant-7bhqbn'

--- src/types.h
+++ src/types.h
@@ -822,7 +822,22 @@ inline Square gating_square(Move m) {
 }
 
 inline bool is_gating(Move m) {
-  return gating_type(m) && (type_of(m) == NORMAL || type_of(m) == CASTLING);
+  if (!gating_type(m))
+      return false;
+
+  MoveType mt = type_of(m);
+  if (mt == NORMAL || mt == CASTLING || mt == EN_PASSANT)
+      return true;
+
+  if (mt == PROMOTION || mt == PIECE_PROMOTION)
+  {
+      Square gate = gating_square(m);
+      Square from = from_sq(m);
+      Square to = to_sq(m);
+      return gate == from || gate == to;
+  }
+
+  return false;
 }
 
 inline bool is_pass(Move m) {
