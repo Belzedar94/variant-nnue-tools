e3d712b2a9a365419703515a18c6fb62cc97d3bd
David <davidjsmurf@gmail.com>
2026-01-16 12:48:13 +0100

    Fix spell-chess castling freeze and trainer_config piece types

--- src/uci.cpp
+++ src/uci.cpp
@@ -390,7 +390,8 @@ void search_mcts_cmd(Position& pos, istringstream& is)
     const Variant* v = variants.find(variant)->second;
     std::cerr << "Writing config for variant " + variant << std::endl;
 
-    const int nnuePieceTypes = popcount(v->pieceTypes) - (v->nnueKing != NO_PIECE_TYPE ? 1 : 0);
+    const int pieceTypeCount = popcount(v->pieceTypes);
+    const int nnuePieceTypes = pieceTypeCount - (v->nnueKing != NO_PIECE_TYPE ? 1 : 0);
     const int potionBits = v->potions ? COLOR_NB * Variant::POTION_TYPE_NB * (1 + 7 + POTION_COOLDOWN_BITS) : 0;
     const int dataSize = (v->maxFile + 1) * (v->maxRank + 1) + v->nnueMaxPieces * 5
                         + nnuePieceTypes * 2 * 5 + 50 + potionBits > 512 ? 1024 : 512;
@@ -407,7 +408,7 @@ void search_mcts_cmd(Position& pos, istringstream& is)
     *varianth
     << "#define FILES " << v->maxFile + 1 << std::endl
     << "#define RANKS " << v->maxRank + 1 << std::endl
-    << "#define PIECE_TYPES " << nnuePieceTypes << std::endl
+    << "#define PIECE_TYPES " << pieceTypeCount << std::endl
     << "#define PIECE_COUNT " << v->nnueMaxPieces << std::endl
     << "#define POCKETS " << (v->nnueUsePockets ? "true" : "false") << std::endl
     << "#define KING_SQUARES " << v->nnueKingSquare << std::endl
@@ -427,7 +428,7 @@ void search_mcts_cmd(Position& pos, istringstream& is)
     << "FILES = " << v->maxFile + 1 << std::endl
     << "SQUARES = RANKS * FILES" << std::endl
     << "KING_SQUARES = " << v->nnueKingSquare << std::endl
-    << "PIECE_TYPES = " << nnuePieceTypes << std::endl
+    << "PIECE_TYPES = " << pieceTypeCount << std::endl
     << "PIECES = 2 * PIECE_TYPES" << std::endl
     << "USE_POCKETS = " << (v->nnueUsePockets ? "True" : "False") << std::endl  
     << "POCKETS = 2 * FILES if USE_POCKETS else 0" << std::endl
