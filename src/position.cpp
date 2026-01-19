da2e49fa6f9869064b452602c31b4e2c22c6ed1d
David <davidjsmurf@gmail.com>
2026-01-19 20:57:19 +0100

    Revert "spell: freeze center only blocks moves"
    This reverts commit f933b85d41352acdef7d5ef93045d1e90f6a88f1.

--- src/position.cpp
+++ src/position.cpp
@@ -1319,7 +1319,8 @@ bool Position::legal(Move m) const {
   SpellContextScope spellScope(*this, freezeExtra, jumpRemoved);
   PieceType royal = royal_piece_type();
 
-  Bitboard frozen = freeze_move_squares();
+  Bitboard frozen = st->potionZones[WHITE][Variant::POTION_FREEZE]
+                  | st->potionZones[BLACK][Variant::POTION_FREEZE];
   if (type_of(m) != DROP && (frozen & from))
       return false;
   if (jumpRemoved && (square_bb(to) & jumpRemoved))
@@ -1522,7 +1523,7 @@ bool Position::legal(Move m) const {
       Square rto = to + (to_sq(m) > from_sq(m) ? WEST : EAST);
       if (is_gating(m) && (gating_square(m) == to || gating_square(m) == rto))  
           return false;
-      if (freeze_move_squares() & to_sq(m))
+      if (freeze_squares() & to_sq(m))
           return false;
 
       // Only the castling king piece is subject to attack checks
